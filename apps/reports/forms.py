cbbaf3ed1070317610f58d1f4643025cba371c9c
donogema <seasicoment@gmail.com>
2025-05-02 20:34:07 +0300

    Add apps/reports/forms.py 1746207247126

---
+++ apps/reports/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class ReportFilterForm(forms.Form):
+    start_date = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
+    end_date = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
+    customer = forms.CharField(required=False, max_length=255)
